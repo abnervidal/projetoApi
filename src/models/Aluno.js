b4f3ce69a2fa9a92d164183152f2577aa72ab997
ABNER VIDAL <89116961+abnervidal@users.noreply.github.com>
2023-03-13 15:58:50 -0300

    Add files via upload

---
+++ src/models/Aluno.js
@@ -0,0 +1,74 @@
+import Sequelize, { Model } from 'sequelize';
+
+export default class Aluno extends Model {
+  static init(sequelize) {
+    super.init({
+      nome: {
+        type: Sequelize.STRING,
+        defaultValue: '',
+        validate: {
+          len: {
+            args: [3, 250],
+            msg: 'Nome precisa ter mais de 3 caracteres!',
+          },
+        },
+      },
+      sobrenome: {
+        type: Sequelize.STRING,
+        defaultValue: '',
+        validate: {
+          len: {
+            args: [3, 250],
+            msg: 'Nome precisa ter mais de 3 caracteres!',
+          },
+        },
+      },
+      email: {
+        type: Sequelize.STRING,
+        defaultValue: '',
+        unique: {
+          msg: 'Email já existe!',
+        },
+        validate: {
+          isEmail: {
+            msg: 'Email Inválido!',
+          },
+        },
+      },
+      idade: {
+        type: Sequelize.INTEGER,
+        defaultValue: '',
+        validate: {
+          isInt: {
+            msg: 'Idade precisa ser um número inteiro!',
+          },
+        },
+      },
+      peso: {
+        type: Sequelize.FLOAT,
+        defaultValue: '',
+        validate: {
+          isFloat: {
+            msg: 'peso precisa ser um número válido!',
+          },
+        },
+      },
+      altura: {
+        type: Sequelize.FLOAT,
+        defaultValue: '',
+        validate: {
+          isFloat: {
+            msg: 'altura precisa ser um número válido!',
+          },
+        },
+      },
+    }, {
+      sequelize,
+    });
+    return this;
+  }
+
+  static associate(models) {
+    this.hasMany(models.Foto, { foreignKey: 'aluno_id' });
+  }
+}
